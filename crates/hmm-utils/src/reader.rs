57519915da14f1f19653450195483e1e06ee96d9
Brixter Porras <brixterporras@gmail.com>
2023-11-03 16:54:49 +0800

    feat: added viewFiles args

--- crates/hmm-utils/src/reader.rs
+++ crates/hmm-utils/src/reader.rs
@@ -3,9 +3,9 @@ use glob::glob;
 use serde::{Deserialize, Serialize};
 
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct FileInfo {
-    pub path: Option<PathBuf>,
+    pub path: PathBuf,
     pub name: String,
     pub size: u64,
     pub file_ext: Option<String>,
@@ -13,36 +13,49 @@ pub struct FileInfo {
     pub mod_time: SystemTime,
 }
 
+#[derive(Debug, Deserialize, Serialize)]
+pub struct ViewFilesOptions {
+    pub path: Option<String>,
+    pub hidden: bool,
+}
 
 pub struct Reader;
 
 impl Reader {
-    pub fn list_files() -> Vec<FileInfo> {
+    pub fn view_files(args:ViewFilesOptions) -> Vec<FileInfo> {
+        let hidden = args.hidden;
+
         let home_dir = dirs::home_dir().expect("Failed to get home directory");
-        let pattern = format!("{}/*", home_dir.to_string_lossy());
+        let root_path = args.path.unwrap_or(home_dir.to_string_lossy().to_string());
+        let pattern = format!("{}/*", root_path);
 
         glob(&pattern)
             .expect("Failed to read glob pattern")
-            .filter_map(|entry| {
+            .filter_map(|entry|{
                 if let Ok(path) = entry {
-                    let metadata = fs::metadata(&path).ok()?;
+                    let metadata = fs::metadata(&path).expect("Failed to read metadata");
                     let file_type = metadata.file_type();
-                    let is_dir = file_type.is_dir();
-                    let file_ext = path.extension()?.to_string_lossy().to_string();
+                    let name= path.file_name().unwrap().to_string_lossy().to_string();
 
-                    Some(FileInfo {
-                        path: Some(path.clone()),
-                        name: path.file_name()?.to_string_lossy().to_string(),
-                        size: metadata.len(),
-                        file_ext: if !is_dir { Some(file_ext) } else { None },
-                        is_dir,
-                        mod_time: metadata.modified().unwrap_or(SystemTime::UNIX_EPOCH),
-                    })
+                    let is_hidden = name.starts_with(".");
+
+                    if hidden || !is_hidden {
+                        Some(FileInfo {
+                            name,
+                            path:path.clone(),
+                            size:metadata.len(),
+                            file_ext:path.extension().and_then(|os_str| os_str.to_str()).map(|s| s.to_string()),
+                            is_dir:file_type.is_dir(),
+                            mod_time:metadata.modified().unwrap_or(SystemTime::UNIX_EPOCH),
+                        })
+                    }else{
+                        None
+                    }
                 } else {
                     None
                 }
-            })
-            .collect()
+            }).collect()
+
     }
 }
 
@@ -52,7 +65,7 @@ mod tests {
 
     #[test]
     fn test_list_files() {
-        let files = Reader::list_files();
+        let files = Reader::view_files(ViewFilesOptions { path: None, hidden: true });
         assert!(!files.is_empty(), "No files were listed");
     }
 }
--- crates/hmm/src/main.rs
+++ crates/hmm/src/main.rs
@@ -1,7 +1,10 @@
-use hmm_utils::reader::Reader;
+use hmm_utils::reader::{Reader, ViewFilesOptions};
 
 fn main() {
-        let file_list = Reader::list_files();
+    let file_list = Reader::view_files(ViewFilesOptions{
+        path:None,
+        hidden:false
+    });
 
     for file in file_list {
         println!("{:#?}", file);
